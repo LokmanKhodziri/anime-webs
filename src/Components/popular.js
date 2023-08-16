6435ee8a7deaeec0763248b4428ffd1d302aa1f2
lokmankhodziri <lokmankhodziri@gmail.com>
2023-08-16 09:49:28 +0800

    adding popular page from api

---
+++ src/Components/popular.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useGlobalContext } from "../context/global.context";
+import { Link } from "react-router-dom";
+
+function Popular() {
+    const { popularAnime, isSearch } = useGlobalContext()
+
+    const conditionalRender = () => {
+        if (!isSearch) {
+            return popularAnime.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
+        }
+    }
+
+    return (
+        <div>
+            <div className="popular-anime">
+                {conditionalRender()}
+            </div>
+        </div>
+    )
+}
+
+export default Popular;
